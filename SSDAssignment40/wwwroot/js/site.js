c495c634f22ccd21dd05211bf3510c5b3f93b180
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-07-07 17:27:22 +0800

    Added auto fade for login/logout alerts.

--- SSDAssignment40/wwwroot/js/site.js
+++ SSDAssignment40/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+(setTimeout(
+        function () {
+            let log_alert = document.getElementById("log_alert");
+            if (log_alert != null) {
+                document.getElementById("close_alert").click();
+            }
+        }, 2500));
