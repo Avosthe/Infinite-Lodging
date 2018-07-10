7ae1ea4aa9a2223775fd0957b1a61d65128487aa
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-07-10 16:47:34 +0800

    Added authorization for identity/account folder

--- SSDAssignment40/Startup.cs
+++ SSDAssignment40/Startup.cs
@@ -64,6 +64,11 @@ namespace SSDAssignment40
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // security (authorization) portion
+            services.AddMvc().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AuthorizeAreaFolder("Identity", "/Account");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
