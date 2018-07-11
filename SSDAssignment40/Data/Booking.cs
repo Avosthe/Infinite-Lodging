e66fc688f6903582daa83b3e2c6b77c7ff31dbc6
Darren Chan <s10179845@connect.np.edu.sg>
2018-07-11 22:04:48 +0800

    Added Booking Listings Review models, added profilepic attribute to lodger

---
+++ SSDAssignment40/Data/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SSDAssignment40.Data
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public int ListingID { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public int LodgerID { get; set; }
+
+        public Lodger Lodger { get; set; }
+        public Listing Listing { get; set; }
+    }
+}
---
+++ SSDAssignment40/Data/Migrations/20180711135746_BookingListingReview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSDAssignment40.Data.Migrations
+{
+    public partial class BookingListingReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePic",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePic",
+                table: "AspNetUsers");
+        }
+    }
+}
