3234e9daae3eeee6768bbb3184e767e6bb021314
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-07-13 19:33:30 +0800

    Renamed Errors folder to Error. Added 403 http error custom page. Added Index and Edit pages under the Profile Folder. Created profile-images folder under wwwroot. Changed folder directory that defines where http error code pages are stored

---
+++ SSDAssignment40/Pages/Profile/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SSDAssignment40.Data;
+
+namespace SSDAssignment40.Pages
+{
+
+    [Authorize]
+    public class ProfileModel : PageModel
+    {
+        [FromRoute]
+        public string Username { get; set; }
+        [BindProperty]
+        public string UserID { get; set; }
+
+        public string ProfilePicture { get; set; }
+        public UserManager<Lodger> _userManager { get; set; }
+
+        public bool isValidProfile { get; set; }
+
+        public bool isEditing { get; set; }
+
+        public ProfileModel(UserManager<Lodger> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Lodger l = await _userManager.FindByNameAsync(Username);
+            if (l is Lodger) isValidProfile = true;
+            return Page();
+        }
+    }
+}
