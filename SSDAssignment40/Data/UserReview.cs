366ce7967beb6040102430774630f345333a7fde
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-07-14 17:15:23 +0800

    Added default values for Lodger attributes upon registration. Changed _Layout.cshtml belonging to Identity/Account. Added Hobbies, GovernmentID properties to Lodger but is still commented out for now. Added much to Edit and Index for Profile

---
+++ SSDAssignment40/Data/UserReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSDAssignment40.Data
+{
+    public class UserReview
+    {
+        public string ReviewId { get; set; }
+        public string ReviewFor { get; set; }
+        public string ReviewBy { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
