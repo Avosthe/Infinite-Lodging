8b60eeabdadde6c12a55be752c4d88a0ead499c2
Chia Zhi Peng <s10179474@connect.np.edu.sg>
2018-07-16 19:48:02 +0800

    Added CreditCard Class, payment implementation

---
+++ SSDAssignment40/Pages/PaymentMethod/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SSDAssignment40.Data;
+
+namespace SSDAssignment40.Pages.PaymentMethod
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SSDAssignment40.Data.ApplicationDbContext _context;
+
+        public DeleteModel(SSDAssignment40.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CreditCard CreditCard { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CreditCard = await _context.CreditCard.FirstOrDefaultAsync(m => m.CreditCardId == id);
+
+            if (CreditCard == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CreditCard = await _context.CreditCard.FindAsync(id);
+
+            if (CreditCard != null)
+            {
+                _context.CreditCard.Remove(CreditCard);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
