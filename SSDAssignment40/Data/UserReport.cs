49d743857c67ebd64e947a2cfb665f87fb9a8072
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-07-18 19:13:09 +0800

    Added UserReport Class. Adding full report functionality. Yet to implement viewing of user reports (for administrators only).

---
+++ SSDAssignment40/Data/UserReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSDAssignment40.Data
+{
+    public class UserReport
+    {
+        public string UserReportId { get; set; }
+        public virtual Lodger ReportedUser { get; set; }
+        public virtual Lodger ReportingUser { get; set; }
+        public string ReportedContent { get; set; }
+        public string ReportFileEvidence { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool isReviewd { get; set; }
+    }
+}
