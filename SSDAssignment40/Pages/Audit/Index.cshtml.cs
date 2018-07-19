b8425e106d713f935503f7e2fd0a602123831002
Yao Wei Zachary Chng <s10179031@connect.np.edu.sg>
2018-07-19 12:36:21 +0800

    Added audit class, migration and CRUD

---
+++ SSDAssignment40/Pages/Audit/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SSDAssignment40.Data;
+
+namespace SSDAssignment40.Pages.Audit
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SSDAssignment40.Data.ApplicationDbContext _context;
+
+        public IndexModel(SSDAssignment40.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<AuditRecord> AuditRecord { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            AuditRecord = await _context.AuditRecords.ToListAsync();
+        }
+    }
+}
