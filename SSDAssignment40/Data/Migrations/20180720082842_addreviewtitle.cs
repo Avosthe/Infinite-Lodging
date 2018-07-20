d8155a4e357e29702678cb85c54db299056515ba
Tze Hao Darren Chan <s10179845@connect.np.edu.sg>
2018-07-20 16:30:17 +0800

    added migration for the addition of review title attribute

---
+++ SSDAssignment40/Data/Migrations/20180720082842_addreviewtitle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSDAssignment40.Data.Migrations
+{
+    public partial class addreviewtitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewTitle",
+                table: "Review",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewTitle",
+                table: "Review");
+        }
+    }
+}
