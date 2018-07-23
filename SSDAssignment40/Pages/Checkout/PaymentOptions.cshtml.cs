e62827bb61eb9623192f2047f6b228e8610a240d
Chia Zhi Peng <s10179474@connect.np.edu.sg>
2018-07-23 21:35:51 +0800

    Added jquery for paymentoptions

--- SSDAssignment40/Pages/Checkout/PaymentOptions.cshtml.cs
+++ SSDAssignment40/Pages/Checkout/PaymentOptions.cshtml.cs
@@ -5,17 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using SSDAssignment40.Pages;
+using SSDAssignment40.Data;
 
 namespace SSDAssignment40.Pages.Checkout
 {
     public class PaymentOptionsModel : PageModel
     {
-       
+
         public void OnGet()
         {
-             
 
+        }
+        public IList<CreditCard> CreditCard { get; set; }
 
     }
-}
 }
