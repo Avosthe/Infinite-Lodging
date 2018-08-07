71d2242988f5fd34be9d68509cdcca5e529f3fb8
Tze Hao Darren Chan <s10179845@connect.np.edu.sg>
2018-08-07 15:48:11 +0800

    added cancellation of bookings and viewing of users bookings

---
+++ SSDAssignment40/Pages/BookingDetails.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SSDAssignment40.Data;
+
+namespace SSDAssignment40.Pages
+{
+    public class DetailsModelBooking : PageModel
+    {
+        private readonly SSDAssignment40.Data.ApplicationDbContext _context;
+
+        public DetailsModelBooking(SSDAssignment40.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Booking Booking { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Booking = await _context.Booking.FirstOrDefaultAsync(m => m.BookingId == id);
+
+            if (Booking == null)
+            {
+                return NotFound();
+            }
+
+            TotalPrice = Booking.Listing.Price * (int)(Booking.DateEnd - Booking.DateStart).TotalDays;
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Booking = await _context.Booking.FindAsync(id);
+
+            if (Booking != null)
+            {
+                _context.Booking.Remove(Booking);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./MyBookings");
+        }
+    }
+}
---
+++ SSDAssignment40/Pages/MyBookings.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SSDAssignment40.Data;
+
+namespace SSDAssignment40
+{
+    public class MyBookingsModel : PageModel
+    {
+        private readonly SSDAssignment40.Data.ApplicationDbContext _context;
+
+        public UserManager<Lodger> _userManager { get; set; }
+
+        public MyBookingsModel(SSDAssignment40.Data.ApplicationDbContext context, UserManager<Lodger> usermanager)
+        {
+            _context = context;
+            _userManager = usermanager;
+        }
+
+        public IList<Booking> Booking { get;set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (_userManager.GetUserId(User) == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = from r in _context.Booking where r.Lodger.Id == _userManager.GetUserId(User) select r;
+            Booking = await bookings.ToListAsync();
+            return Page();
+        }
+    }
+}
